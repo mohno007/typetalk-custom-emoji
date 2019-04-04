ac89bbce2ec1c5c53e1fc269b8e91f0175c2bac4
Motohiro Ohno <m-ohno@atware.co.jp>
2019-04-04 21:56:06 +0900

    Enhance index.js with Emoji and Emojis

--- src/emoji_parser.js
+++ src/emoji_parser.js
@@ -15,6 +15,29 @@ export class EmojiParser {
     return (this.re = new RegExp(`:(${codeList.join('|')}):`, 'g'));
   }
 
+  parseText(text) {
+    const gene = this.parse(text);
+
+    const result = [];
+    let index = 0;
+
+    for (
+      let { done, value } = gene.next();
+      !done;
+      { done, value } = gene.next()
+    ) {
+      const { emoji, leftEndIndex, rightStartIndex } = value;
+
+      result.push(text.substring(index, leftEndIndex));
+      result.push(emoji);
+
+      index = rightStartIndex;
+    }
+    result.push(text.substring(index));
+
+    return result;
+  }
+
   *parse(text) {
     const regex = this.regex;
 
