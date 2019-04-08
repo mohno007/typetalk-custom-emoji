dd6aa4f1a0615399153a6354722a8f84b91bf11e
Моtоhіrо Оno <31834844+mohno007@users.noreply.github.com>
2019-04-08 14:13:14 +0900

    Fix EmojiParser test name

--- test/emoji_parser.test.js
+++ test/emoji_parser.test.js
@@ -3,7 +3,7 @@ import { Emojis } from '../src/emojis.js';
 import { Emoji } from '../src/emoji.js';
 import Assert from 'assert';
 
-describe('Emoji', function() {
+describe('EmojiParser', function() {
   const code = 'smiley';
   const url =
     'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEUAAAD+/v7KB/Q9AAAAFklEQVQI12P4z8TALMUAIu2Y2Bj+AwAbxwNogPCregAAAABJRU5ErkJggg==';
